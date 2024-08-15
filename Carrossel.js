783334b9618f5b622b97dab9692fdc1436833906
GustavoHenrique444 <gustavo.souza532@etec.sp.gov.br>
2024-08-15 11:46:25 -0300

    Adicionando Codigos em Java Script

---
+++ Carrossel.js
@@ -0,0 +1,11 @@
+var radio = document.querySelector('.manual-btn')
+var cont = 1
+document.getElementById('radio1').checked = true
+
+setInterval(() => {
+    proximaImg()
+}, 5000)
+
+function proximaImg(){
+
+}
